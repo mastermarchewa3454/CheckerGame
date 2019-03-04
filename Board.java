96c2b11f5d5fb918449818287d6bfc9c8b8c07b3
MasterMarchewa69 <piter292@amorki.pl>
2019-03-04 18:24:20 +0000

    Creating a basic board + downloading all elements

---
+++ Board.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+public class Board
+{
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+    public Board()
+    {
+        frame.setContentPane(panel);
+        frame.setSize(1000,1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setVisible(true);
+    }  
+
+    public static void main (String [] args)
+    {
+        Board board = new Board();
+        how to 
+    }
+}
---
+++ Square.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+public class Square
+{
+   Board board = new Board();
+}
+public
