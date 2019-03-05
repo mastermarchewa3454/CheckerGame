5f07bbde90715410dc66699e8b0c8420d6c924bc
MasterMarchewa69 <piter292@amorki.pl>
2019-03-05 19:40:10 +0000

    Make blackButtons JLabel (their do not actually play)

--- Square.java
+++ Square.java
@@ -1,7 +1,41 @@
 import javax.swing.*;
-import java.awt.*;
-public class Square
+
+import javax.swing.JFrame;
+
+public class Square extends JFrame
 {
-   Board board = new Board();
+
+public Square()
+{
+   JButton[] blackButtons = new JButton[4*8];
+   JButton[] whiteButtons = new JButton[4*8];
+   for (int i=0; i<blackButtons.length; i++)
+   {
+      blackButtons[i] = new JButton(" ");
+   }
+   for (int i=0; i<whiteButtons.length; i++)
+   {
+      whiteButtons[i] = new JButton(" ");
+   }
+   for (int i=0; i<8; i++)
+   {
+      if(i%2 ==0)
+      {
+         for (int j=0; j<4; j++)
+         {
+            add(blackButtons[4*i +j]);
+            add(whiteButtons[4*i +j]);
+         }
+      }
+      else
+      {
+         for (int j=0; j<4; j++)
+         {
+            add(whiteButtons[4*i +j]);
+            add(blackButtons[4*i +j]);
+         }
+      }
+   }
 }
-public
+
+}
