d810d9b8f8958ca234678cd657f608c7ffcc1e3e
MasterMarchewa69 <piter292@amorki.pl>
2019-03-06 19:17:31 +0000

    Setting Piece and organizing code

--- Board.java
+++ Board.java
@@ -3,11 +3,47 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 public class Board extends JFrame
 {
+   private JFrame frame = new JFrame();
+   private JPanel panel = new JPanel();
+   private int width = 8;
+   public Board()
+   {
+      Square square = new Square();
+      JLabel[] blackButtons = square.getblackButtons();
+      JButton[] whiteButtons = square.getwhiteButtons();
+      panel.setLayout(new GridLayout(width, width));
+      panel.setVisible(true);
+      square.setSquare();
+      setSize(600, 600);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+      for (int i = 0; i < width; i++) 
+      {
+         
+         if (i % 2 == 0) 
+         {
+            for (int j = 0; j < 4; j++) 
+            {
+               panel.add(blackButtons[4 * i + j]);
+               panel.add(whiteButtons[4 * i + j]);  
+            }
+         } 
+         else 
+         {
+            for (int j = 0; j < 4; j++) 
+            {
+               
+               panel.add(whiteButtons[4 * i + j]);
+               panel.add(blackButtons[4 * i + j]);
+               
+            }
+         }
+      }
+      super.add(panel);
+   }
+   
     public static void main (String [] args)
     {
-        Square square = new Square();
-        square.setSize(600,600);
-        square.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        square.setVisible(true);
+        Board board = new Board();
     }
 }
--- Square.java
+++ Square.java
@@ -9,58 +9,81 @@ public class Square extends JFrame {
    private JPanel panel = new JPanel();
    private JLabel[] blackButtons = new JLabel[4 * 8];
    private JButton[] whiteButtons = new JButton[4 * 8];
+   private int[] whiteCounter = new int[12];
+   private int[] redCounter = new int[12];
+   private int width = 8;
 
-   public JLabel[] getblackButtons()
-   {
+   public JLabel[] getblackButtons() {
       return blackButtons;
 
    }
-   public JButton[] getwhiteButtons()
-   {
+
+   public JButton[] getwhiteButtons() {
       return whiteButtons;
    }
-   public void setblackButtons(JLabel[] bb)
-   {
+
+   public void setblackButtons(JLabel[] bb) {
       this.blackButtons = bb;
-   } 
-   public void setwhiteButtons(JButton[] wb)
-   {
+   }
+
+   public void setwhiteButtons(JButton[] wb) {
       this.whiteButtons = wb;
    }
 
-    public Square()
-    {
-        panel.setLayout(new GridLayout(8,8));
-        panel.setSize(1000,1000);
-        for (int i=0; i<blackButtons.length; i++)
-        {
-           ImageIcon im = new ImageIcon("empty2.png");
-           blackButtons[i] = new JLabel(im);
-        }
-        for (int i=0; i<whiteButtons.length; i++)
-        {
-           ImageIcon im = new ImageIcon("empty.png");
-           whiteButtons[i] = new JButton(im);
-        }
-        for (int i=0; i<8; i++)
-        {
-           if(i%2 ==0)
-           {
-              for (int j=0; j<4; j++)
-              {
-                 panel.add(blackButtons[4*i +j]);
-                 panel.add(whiteButtons[4*i +j]);
-              }
-           }
-           else
-           {
-              for (int j=0; j<4; j++)
-              {
-                 panel.add(whiteButtons[4*i +j]);
-                 panel.add(blackButtons[4*i +j]);
-              }
-           }
-        }
-        super.add(panel);
-     } 
+   public void setSquare() {
+     // panel.setLayout(new GridLayout(width, width));
+    // panel.setSize(1000, 1000);
+      for (int i = 0; i < blackButtons.length; i++) {
+         ImageIcon im = new ImageIcon("empty2.png");
+         blackButtons[i] = new JLabel(im);
+      }
+      for (int i = 0; i < whiteButtons.length; i++) {
+         ImageIcon im = new ImageIcon("empty.png");
+         whiteButtons[i] = new JButton(im);
+         if(i >19)
+         {
+            WhitePieces(i);
+         }
+         if (i <12)
+         {
+            RedPieces(i);
+         }
+      }
+   }
+    /*  for (int i = 0; i < width; i++) 
+      {
+         
+         if (i % 2 == 0) 
+         {
+            for (int j = 0; j < 4; j++) 
+            {
+               panel.add(blackButtons[4 * i + j]);
+               panel.add(whiteButtons[4 * i + j]);  
+            }
+         } 
+         else 
+         {
+            for (int j = 0; j < 4; j++) 
+            {
+               
+               panel.add(whiteButtons[4 * i + j]);
+               panel.add(blackButtons[4 * i + j]);
+               
+            }
+         }
+      }
+      super.add(panel);
+   }
+   */
+
+   public void WhitePieces(int i)
+   {
+         ImageIcon image = new ImageIcon("white.png");
+         whiteButtons[i].setIcon(image);
+   }
+   public void RedPieces(int i)
+   {
+         ImageIcon image = new ImageIcon("red.png");
+         whiteButtons[i].setIcon(image);
+   }
 }
